8121f8a5e8516b2ab1dfcef59e0984625b5945fc
Dim Chen <dimpurr@live.com>
2023-02-12 19:34:24 +0000

    login

--- goshawk-pwa/src/App.js
+++ goshawk-pwa/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+import { Login } from '@microsoft/mgt-react';
+
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: 'b55064f9-3aaf-4e3b-9812-9f0bded04875'
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Login />
       </header>
     </div>
   );
